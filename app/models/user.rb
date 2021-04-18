6d1b21bcbf5fda68e6d638e0b821000b6ae14fa3
EC2 Default User <ec2-user@ip-172-31-69-175.ec2.internal>
2021-04-18 13:36:28 +0000

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ApplicationRecord
   
   attachment :profile_image
   
+  validates :name, uniqueness: true, length: { in: 2..20 }
+  validates :introduction, length: { maximum: 50}
+  
 end
--- db/migrate/20210411142904_create_books.rb
+++ db/migrate/20210411142904_create_books.rb
@@ -3,7 +3,6 @@ class CreateBooks < ActiveRecord::Migration[5.2]
     create_table :books do |t|
       t.string :title
       t.text :body
-      t.text :caption
       t.integer :user_id
 
       t.timestamps
